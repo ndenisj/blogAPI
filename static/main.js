61b8b1cd407516a5fdf5e2239037f01e51348e68
Denis John <denis.john.ndarake@gmail.com>
2019-08-19 23:29:05 +0100

    Used Jquery Ajax to fetch the data once the home url is loaded

--- static/main.js
+++ static/main.js
@@ -2,6 +2,21 @@ var btn = document.getElementById("btn");
 var container = document.getElementById("container");
 var url = 'http://127.0.0.1:8000';
 
+// Using jquery Ajax
+$.ajax({
+    method: 'GET',
+    url: url,
+    success: function(data){
+        console.log(data);
+        console.log("Success");
+    },
+    error: function (error_data) {
+        console.log("Error");
+    }
+});
+
+
+// Using vanilla javascript
 btn.addEventListener("click", function () {
     var ourRquest = new XMLHttpRequest();
     ourRquest.open("GET", url);
